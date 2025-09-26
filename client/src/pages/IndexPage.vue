3b83eb3fb5aafc6d6cdef47c07698ae6256a311a
adam-alberty <alberty.adam@proton.me>
2025-09-26 22:17:32 +0200

    feat(chat): add message input to chat interface

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <q-page class="row items-center justify-evenly text-grey-7 text-h6">
-    Select a channel to start messaging
+  <q-page>
+    <div class="row items-center justify-evenly text-grey-7 text-h6 q-py-xl">
+      Select a channel to start messaging
+    </div>
+
+    <div class="absolute-bottom">
+      <textarea class="command-input bg-grey-3" placeholder="Type a message"></textarea>
+    </div>
   </q-page>
 </template>
 
