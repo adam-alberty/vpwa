b0ae46d306d7c6bada8433e76bfe1436d11f2c6e
adam-alberty <xalberty@stuba.sk>
2025-09-29 16:07:51 +0200

    chore: add channel message

---
+++ client/src/components/ChannelMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-item class="items-start">
+    <q-item-section avatar>
+      <q-avatar size="32px" color="grey-5" text-color="white">{{ 'name'.charAt(0) }}</q-avatar>
+    </q-item-section>
+
+    <q-item-section>
+      <div>
+        <div class="underline">{{ 'name' }}</div>
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta harum nisi quo molestiae,
+          et qui deleniti neque expedita iure voluptas saepe tempora consequuntur dignissimos earum
+          dolor nemo, obcaecati, accusamus quos.
+        </div>
+      </div>
+    </q-item-section>
+  </q-item>
+</template>
--- client/src/pages/ChannelPage.vue
+++ client/src/pages/ChannelPage.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page>
-    <div class="row items-center justify-evenly text-grey-7 text-h6 q-py-xl">Channel</div>
+    <q-scroll-area style="height: 500px">
+      <ul>
+        <channel-message />
+      </ul>
+    </q-scroll-area>
 
     <div class="absolute-bottom">
       <textarea class="command-input bg-grey-3" placeholder="Type a message"></textarea>
@@ -8,4 +12,6 @@
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChannelMessage from 'src/components/ChannelMessage.vue';
+</script>
