f80e1b7dd6044ba7b3bb6818ab1e37885b539345
Programmer001 <petko.hupka@gmail.com>
2025-11-02 22:19:11 +0100

    Style: Make channel invite more compact

--- frontend/src/components/ChannelInviteCard.vue
+++ frontend/src/components/ChannelInviteCard.vue
@@ -1,14 +1,16 @@
 <template>
-  <q-item class="items-start bg-grey-9">
+  <q-item class="bg-grey-9">
     <q-item-section>
       <div>
-        <q-badge color="blue"> Invite </q-badge>
+        <q-icon name="mail" color="primary" size="19px" class="q-my-none" />
         <Channel-Name v-bind="props" highlight />
 
-        <div class="row q-gutter-x-sm">
-          <q-btn size="sm" dense flat label="Accept" icon="check" class="bg-green-7 text-white q-pr-sm" @click="accept" />
-          <q-btn size="sm" dense flat label="Reject" icon="close" class="bg-red-5 text-white q-pr-sm" @click="reject"/>
-        </div>
+        <q-menu anchor="center end" self="center left" class="no-shadow">
+          <div class="row q-gutter-x-sm q-pa-xs bg-blue-grey-8">
+            <q-btn size="11px" dense flat icon="check" class="bg-positive text-white" @click="accept" />
+            <q-btn size="11px" dense flat icon="close" class="bg-negative text-white" @click="reject"/>
+          </div>
+        </q-menu>
       </div>
     </q-item-section>
   </q-item>
@@ -36,3 +38,8 @@ function reject() {
   emit('reject', props);
 }
 </script>
+
+<style lang="sass" scoped>
+.q-item
+  border-left: 3px solid var(--q-primary)
+</style>
